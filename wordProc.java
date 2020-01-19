e5442307031a4209402685c710d2d6af6cbbfd32
chinkley <59537837+chinkley@users.noreply.github.com>
2020-01-19 17:35:30 -0500

    Update wordProc.java

--- wordProc.java
+++ wordProc.java
@@ -1,9 +1,10 @@
-/*ID: 02charl1
+/*ID: charliehinkley
 TASK: word
 LANG: JAVA
 */
 
 //PROBLEM ID        :   Word Processor
+
 
 import java.io.*;
 import java.util.*;
@@ -13,40 +14,48 @@ public class wordProc
 
 		public static void main(String[] args) throws IOException
 		{
-			//Takes file input
+			//File input and output
 			Scanner scan = new Scanner (new File("word.in"));
 			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("word.out")));
-			
+         
 			//Declares and input values for variables
-			 int numWords = 0, numChar = 0, counter = 0, counter2 = 0;
-			 numWords = scan.nextInt();
-			 numChar = scan.nextInt();
-			 scan.nextLine();
-
-			 String[] input = new String[numWords];    
-
-			 for (int i = 0; i < input.length; i++) {
-			    input[i] = scan.next();
-			 }
-
-			 for (int j = 0; j < input.length; j++) {
-			    if (input[j].length() + counter <= numChar) {
-			       System.out.print(input[j]);
-			       counter += input[j].length();
-
-			       if (j+1 < input.length) {
-				  if (input[j+1].length() + counter <= numChar) {
-				     System.out.print(" ");
-				  }
-			       }
-			    }            
-			    else {
-			       System.out.println("");
-			       counter = 0;
-			       j--;
-			    }
-			 }
+         int numWords = 0, numChar = 0, counter = 0, counter2 = 0;
+         //Reads in number of words and the character limit
+         numWords = scan.nextInt();
+         numChar = scan.nextInt();
+         scan.nextLine();
          
+         String[] input = new String[numWords];    
          
+         //Reads in the string into string array
+         for (int i = 0; i < input.length; i++) {
+            input[i] = scan.next();
+         }
+         
+         //Counts the number of characters in each word
+         //And decides if they can be on one line
+         for (int j = 0; j < input.length; j++) {
+            if (input[j].length() + counter <= numChar) {
+               out.print(input[j]);
+               counter += input[j].length();
+               
+               //Adds a space between words, but not at end of line
+               if (j+1 < input.length) {
+                  if (input[j+1].length() + counter <= numChar) {
+                     out.print(" ");
+                  }
+               }
+            }  
+            //Moves to next line, takes j back to use the last string tested, 
+            //And sets counter to 0        
+            else {
+               out.println("");
+               counter = 0;
+               j--;
+            }
+         }
+         
+         out.close();
+         scan.close();
       }
 }
