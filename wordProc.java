1cf9738b7e01e76fe908683600ddbdac77440a0c
chinkley <59537837+chinkley@users.noreply.github.com>
2020-01-19 12:19:22 -0500

    Update wordProc.java

--- wordProc.java
+++ wordProc.java
@@ -16,37 +16,36 @@ public class wordProc
 			//Takes file input
 			Scanner scan = new Scanner (new File("word.in"));
 			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("word.out")));
-         //Scanner scan = new Scanner(System.in);
 			
 			//Declares and input values for variables
-         int numWords = 0, numChar = 0, counter = 0, counter2 = 0;
-         numWords = scan.nextInt();
-         numChar = scan.nextInt();
-         scan.nextLine();
-         
-         String[] input = new String[numWords];    
-         
-         for (int i = 0; i < input.length; i++) {
-            input[i] = scan.next();
-         }
-         
-         for (int j = 0; j < input.length; j++) {
-            if (input[j].length() + counter <= numChar) {
-               System.out.print(input[j]);
-               counter += input[j].length();
-               
-               if (j+1 < input.length) {
-                  if (input[j+1].length() + counter <= numChar) {
-                     System.out.print(" ");
-                  }
-               }
-            }            
-            else {
-               System.out.println("");
-               counter = 0;
-               j--;
-            }
-         }
+			 int numWords = 0, numChar = 0, counter = 0, counter2 = 0;
+			 numWords = scan.nextInt();
+			 numChar = scan.nextInt();
+			 scan.nextLine();
+
+			 String[] input = new String[numWords];    
+
+			 for (int i = 0; i < input.length; i++) {
+			    input[i] = scan.next();
+			 }
+
+			 for (int j = 0; j < input.length; j++) {
+			    if (input[j].length() + counter <= numChar) {
+			       System.out.print(input[j]);
+			       counter += input[j].length();
+
+			       if (j+1 < input.length) {
+				  if (input[j+1].length() + counter <= numChar) {
+				     System.out.print(" ");
+				  }
+			       }
+			    }            
+			    else {
+			       System.out.println("");
+			       counter = 0;
+			       j--;
+			    }
+			 }
          
          
       }
